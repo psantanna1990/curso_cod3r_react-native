79df9535a415e114e3271c95fe479571ac1bd48a
Pedro Oliveira <pedrosantanna1990@gmail.com>
2020-11-23 13:40:01 -0300

    Adicionando icones a TabView

--- exemploNavegacao/src/navigation/Tab.js
+++ exemploNavegacao/src/navigation/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
@@ -10,10 +11,38 @@ const Tab = createBottomTabNavigator();
 export default (props) => {
   return (
     <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'TelaA':
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+              break;
+
+            case 'TelaB':
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+              break;
+
+            case 'TelaC':
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+              break;
+
+            default:
+              break;
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
       tabBarOptions={{
         activeTintColor: 'red',
         inactiveTintColor: 'blue',
-        labelStyle: {fontSize: 30},
+        showLabel: true,
       }}>
       <Tab.Screen name="TelaA" component={TelaA} />
       <Tab.Screen name="TelaB" component={TelaB} />
